a7fe97167e2f76b71fa2763a3c315b7a61500b5b
Igorexa <Igoryan818@yandex.ru>
2021-11-03 18:14:42 +0300

    added the function of selecting the active slide

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
-const slides = document.querySelectorAll('.slide');
+function slidesPlagin(activeSlide = 0) { 
+    const slides = document.querySelectorAll('.slide');
 
-slides.forEach(slide => {
-        slide.addEventListener('click', () => {
-            clearAcctiveClasses();
-        
-            slide.classList.add('active')
+    slides[activeSlide].classList.add('active'); //выбор активного слайда;
+
+    slides.forEach(slide => {
+            slide.addEventListener('click', () => {
+                clearAcctiveClasses();
+            
+                slide.classList.add('active')
+            })
+        }
+    )
+    
+    function clearAcctiveClasses () {
+        slides.forEach(slide => {
+            slide.classList.remove('active')
         })
     }
-)
 
-function clearAcctiveClasses () {
-    slides.forEach(slide => {
-        slide.classList.remove('active')
-    })
 }
+
+slidesPlagin(0);
+
